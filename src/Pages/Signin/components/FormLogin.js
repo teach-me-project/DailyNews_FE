54fe230e2d71337d2964b8f0afccf25e47170029
dzakia-st3 <annisadzakiaramadhina22@gmail.com>
2022-07-22 14:46:04 +0700

    feat : responsive & redesign UI

---
+++ src/Pages/Signin/components/FormLogin.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import google from '../../../components/images/Google.svg'
+import facebook from '../../../components/images/Facebook.svg'
+import twitter from '../../../components/images/Twitter.svg'
+import { Link } from "react-router-dom";
+import login from '../../../components/images/login-vector.jpg';
+
+const FormLogin = () => {
+  return (
+    <>
+    <div className='form-login'>
+        <div className="flex flex-row">
+            <div className="flex-1">
+                <h1 className="text-4xl font-['Mulish'] mb-6">Login</h1>
+                <form className="">
+                    <div>
+                        <p className=" font-['Mulish'] text-base font-semibold text-[rgba(56, 128, 135, 1)] leading-5 mb-3">Email Address :</p>
+                        <input type="text"
+                            className="form-control block w-[400px] px-2 py-1 text-xl font-normal text-[rgba(56, 128, 135, 1)]-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-[10px] transition ease-in-out mb-5 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="exampleFormControlInput2" placeholder="Enter your email address" />
+                    </div>
+                    <div>
+                        <p className="d-block font-['Mulish'] text-base font-semibold text-[#4E4B66] leading-3 mb-3">Password :</p>
+                        <input type="text"
+                        className="form-control block w-[400px] px-3 py-1 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-[10px] transition ease-in-out  mb-6 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        id="exampleFormControlInput2" placeholder="Enter your password" />
+                    </div>
+                </form>
+                <div className="button mb-8">
+                    <button type="button"
+                    className="inline-block w-[400px] font-['Mulish'] h-10 py-2 bg- text-white font-bold text-xl hover:text-black leading-tight  rounded-lg shadow-md hover:bg-[#72b7e3] hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 bg-[#092a44] active:shadow-lg transition duration-150 ease-in-out">
+                    Login
+                    </button>
+                </div>
+                <div className="inline-block w-[400px] font-['Inter'] text-[#8692A6] leading-5 text-center">
+                    <div className="mb-4 font-['Mulish']">OR SIGN UP WITH</div>
+                    <div className="flex flex-row justify-center mb-5">
+                    <img className="mr-6 w- h-8" src={google} alt="google" />
+                    <img className="mr-6 w-9 h-8" src={facebook} alt="facebook" />
+                    <img className=" w-9 h-auto" src={twitter} alt="twitter" />
+                </div>
+                <p className="mb-5 font-['Mulish']" >
+                already have account?
+                </p> <hr />
+                <button type="button"
+                className="inline-block w-[400px] font-['Mulish'] h-12 py-2.5 bg- text-white font-bold text-xl hover:text-white leading-tight  rounded-lg shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 bg-[#0D253C] active:shadow-lg transition duration-150 ease-in-out">
+                Sign Up Now
+                </button>
+                <Link to={'#'}>
+                    <p className="text-black underline mt-7 font-semibold font-['Mulish']">
+                    Back to Home Page
+                    </p>
+                </Link>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className='form-login-mobile'>
+        <div className='header-mobile'>News Today!</div>
+        <div className='image-login'>
+            <img src={login} alt='login'/>
+        </div>
+        <div className='top-navigation'>
+            <div className='login-mobile'>
+                <h1>LOGIN</h1>
+                <hr/>
+            </div>
+            <div className='login-mobile'>
+                <h1>SIGN UP</h1>
+                <hr/>
+            </div>
+        </div>
+        <div className='card-login'>
+            <h1 className='header-card'>Welcome back!</h1>
+            <h2>Sign in with your account</h2>
+            <form>
+                <div>
+                    <p>Email</p>
+                    <input type="email" id="email" placeholder="Enter your email address"/>
+                </div>
+                <div>
+                    <p>Password</p>
+                    <input type="password" id="password" placeholder="Enter your email password" />
+                </div>
+            </form>
+            <Link to = {'#'}>Forgot your password?</Link> 
+            <button className='btn-login-mobile'>Login</button>  
+            <div className='footer-mobile'>
+                <h1>FAST SIGN IN</h1>
+                <div className="footer-mobile-logo">
+                    <img src={google} alt="google" />
+                    <img src={facebook} alt="facebook" />
+                    <img src={twitter} alt="twitter" />
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+    </>
+  )
+}
+
+export {FormLogin}
--- src/Pages/Signin/index.js
+++ src/Pages/Signin/index.js
@@ -1,67 +1,13 @@
-import bg from '../../components/images/nenek.png'
-import google from '../../components/images/Google.svg'
-import facebook from '../../components/images/Facebook.svg'
-import twitter from '../../components/images/Twitter.svg'
-import { Link } from "react-router-dom";
+import { CoverLeft } from './components/CoverLeft'
+import { FormLogin } from './components/FormLogin'
 
 const Signin = () => {
     return (
         <>
-            <div className="flex flex-row">
-
-                <div>
-                    <img className="relative w-[721px] h-[1110px]" src={bg} alt="gambar" />
-                </div>
-
-                <div className="flex-1 ml-[107px] mt-12">
-                    <h1 className="text-5xl font-['Inter'] mb-8">Login</h1>
-                    <form className="">
-                        <div className="">
-                            <p className=" font-['Mulish'] text-base font-semibold text-[#4E4B66] leading-5 mb-3">
-                                Email Address :
-                            </p>
-                            <input type="text"
-                                className="form-control block w-[400px] px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-[10px] transition ease-in-out mb-6 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                id="exampleFormControlInput2" placeholder="Enter your email address" />
-                        </div>
-                        <div>
-                            <p className="d-block font-['Mulish'] text-base font-semibold text-[#4E4B66] leading-5 mb-3">
-                                Password :
-                            </p>
-                            <input type="text"
-                                className="form-control block w-[400px] px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-[10px] transition ease-in-out  mb-6 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                id="exampleFormControlInput2" placeholder="Enter your password" />
-                        </div>
-                    </form>
-                    <div className="button mb-8">
-                        <button type="button"
-                            className="inline-block w-[400px] font-['Mulish'] h-12 py-2.5 bg- text-black font-bold text-xl hover:text-white leading-tight  rounded-lg shadow-md hover:bg-purple-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 bg-amber-400 active:shadow-lg transition duration-150 ease-in-out">
-                            Login
-                        </button>
-                    </div>
-                    <div className="inline-block w-[400px] font-['Inter'] text-[#8692A6] leading-5 text-center">
-                        <div className="mb-6">OR SIGN UP WITH</div>
-                        <div className="flex flex-row justify-center mb-9">
-                            <img className="mr-6 w-9 h-8" src={google} alt="google" />
-                            <img className="mr-6 w-9 h-8" src={facebook} alt="facebook" />
-                            <img className=" w-9 h-auto" src={twitter} alt="twitter" />
-                        </div>
-                        <p className="mb-5">
-                            already have account?
-                        </p> <hr />
-                        <button type="button"
-                            className="inline-block w-[400px] font-['Mulish'] h-12 py-2.5 bg- text-white font-bold text-xl hover:text-white leading-tight  rounded-lg shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 bg-[#0D253C] active:shadow-lg transition duration-150 ease-in-out">
-                            Sign Up Now
-                        </button>
-                        <Link to={'#'}>
-                            <p className="text-black underline mt-12 font-semibold">
-                                Back to Home Page
-                            </p>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-
+        <div className='login'>
+            <CoverLeft />
+            <FormLogin />
+        </div>
         </>
     )
 }
